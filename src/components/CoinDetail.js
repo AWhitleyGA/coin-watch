2941a94ad10b4c9647f8fa6e4db94caa2d41beb5
Andrew Whitley <awhitley1233@gmail.com>
2017-12-26 18:43:13 -0500

    add trade history fetching to detail view

--- src/components/CoinDetail.js
+++ src/components/CoinDetail.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
 
-const CoinDetail = (props) => {
-  return (    
-    <div>
-    <h2>{props.coin.symbol}</h2>
-    <p>{props.coin.currentSellPrice}</p>
-    </div>
-  )
+class CoinDetail extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      recentTrades: [],
+      timer: setInterval(this.fetchTradeData.bind(this), 5000)
+    }
+  }
+
+  componentDidMount () {
+    this.fetchTradeData()
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.state.timer)
+  }
+
+  fetchTradeData () {
+    axios.get(`http://api.binance.com/api/v1/aggTrades?symbol=${this.props.coin.symbol}USDT`)
+      .then((response) => {
+        this.setState({
+          recentTrades: response
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>{this.props.coin.symbol}</h2>
+        <p>{this.props.coin.currentSellPrice}</p>
+      </div>
+    )
+  }
 }
 
 
