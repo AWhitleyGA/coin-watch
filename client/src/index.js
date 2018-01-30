e00d8dabe52b0289b1c741becabdd79a2c1cc0c7
Andrew Whitley <awhitley1233@gmail.com>
2018-01-30 02:13:13 -0500

    fix store path

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import Store from './Store'
+import Store from './store'
 
 const store = Store()
 
